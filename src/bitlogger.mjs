6ef0a40590600cc19a629150734a36a43d41c33a
knev <knev@users.noreply.github.com>
2022-10-24 23:13:40 +0200

    feat!: Use BigInt; add log handler.

--- src/bitlogger.mjs
+++ src/bitlogger.mjs
@@ -4,37 +4,59 @@
 const __name = obj => Object.keys(obj)[0];
 // console.log('OUT', __name({variableName}) );
 
+function tagsToBigInt_(ref, obj, ignore= false) {
+	let bigInt = BigInt(0);
+	for (const [t,v] of Object.entries(obj)) {
+		if ( ( ignore || v ) && ref[t])
+			bigInt|= BigInt( ref[t] );			
+		// console.log('0b'+ bigInt.toString(2) );
+	}
+	return bigInt;
+}
 //-------------------------------------------------------------------------------------------------
 
+function handler_default_(str_output) {
+	console.log(str_output)
+}
+
 var LOGR_= (function() {
-	let _tags= {};
-	let _toggled= {};
+	let _handler_log= undefined;
+	let _Bint_tags= BigInt(0);
+	let _Bint_toggled= BigInt(0);
 
 	return {
+		set handler(fx) {
+			_handler_log= fx;
+		},
+		get tags() { return _Bint_tags; },
 		set tags(obj) {
-			_tags= obj;
-			console.log(_tags);
+			_Bint_tags= obj;
 		},
-		put : function(tag, label) {
-			let name= __name(tag);
-			_tags[name]= tag[name];
-			console.log(_tags);
-		},
+		// put : function(tag, label) {
+		// 	let name= __name(tag);
+		// 	_tags[name]= tag[name];
+		// 	console.log(_tags);
+		// },
+		get toggled() { return _Bint_toggled; },
 		set toggled(obj) {
-			_toggled= obj;
-			console.log(_toggled);
+			_Bint_toggled= tagsToBigInt_(_Bint_tags, obj);
+			if (! _handler_log)
+				_handler_log= handler_default_;
 		},
-		log : function(logged, output) {
-			console.log('tags', logged)
-			for (const [t,v] of Object.entries(_tags)) {
-				console.log('e', t, v);
-				if ( (logged&v) && (_toggled[t] !== undefined) )
-					console.log(output)
-			}
+		log : function(nr_logged, str_output) {
+			if (! _handler_log)
+				return false;
+			
+			let Bint_logged= BigInt(nr_logged)
+			if ((Bint_logged & _Bint_toggled) === BigInt(0))
+				return false;
+
+			_handler_log(str_output);
+			return true;
 		}
 	}
 })();
 
 //-------------------------------------------------------------------------------------------------
 
-export { LOGR_ as LOGR };
+export { LOGR_ as LOGR, handler_default_ as console_logr };
--- tests/LOGR.test.mjs
+++ tests/LOGR.test.mjs
@@ -1,69 +1,83 @@
 
-import { LOGR } from '../src/bitlogger.mjs';
+import { LOGR as LOGR_, console_logr as console_logr_ } from '../src/bitlogger.mjs';
 
 var options= {
 	logr : {
 		CXNS : 1,
 		MSGE : 1,
-		DUPS : 1
+		DUPS : 0,
+		RELF : 1,
 	}
 }
 
 //-------------------------------------------------------------------------------------------------
 
+const REFL= 0b1 << 1;	// reflection
+
 const l_ = {
 	CXNS : 0b1 << 0,	// connections
-	REFL : 0b1 << 1,	// reflection
 	DUPS : 0b1 << 2,	// duplicates
 	MSGE : 0b1 << 3,	// MsgEnv
 }
 
-LOGR.tags= l_;
+// LOGR_.put({REFL}, 'FL');
+
+LOGR_.tags= l_;
+console.log('LOGR_.tags', LOGR_.tags);
+
+LOGR_.toggled= options.logr;
+console.log('LOGR_.toggled', LOGR_.toggled.toString(2) );
+console.log()
 
 // ----
 
-// LOGR_.put({CXNS}, 'XN');
-// LOGR_.put({REFL}, 'FL');
-
-LOGR.logr= options.logr;
+LOGR_.log(l_.CXNS, 'YES');
 console.log()
 
-// LOGR.log(l_.CXNS, 'YES');
-// console.log()
-
-// LOGR.log(l_.REFL, 'NO');
-// console.log()
-
-// let bigInt = BigInt(0);
-// console.log(bigInt);
-
-// bigInt|= BigInt(l_.MSGE);
-// console.log(bigInt);
-
-function tagsToBigInt(obj) {
-	let bigInt = BigInt(0);
-	for (const [t,v] of Object.entries(obj)) {
-		if (v)
-			bigInt|= BigInt( l_[t] );			
-		// console.log('0b'+ bigInt.toString(2) );
-	}
-	return bigInt;
-}
-
-let x= tagsToBigInt(options.logr);
-console.log(x.toString(2));
+LOGR_.log(l_.DUPS, 'NO');
+console.log()
 
 //-------------------------------------------------------------------------------------------------
 
 const ll_= {
 	DEL : 0b1 << 0,		// removed
-	CXNS : 0b1 << 3,	// connections
+	CXNS : 0b1 << 2,	// connections
 }
 
-// LOGR.tags= ll_;
+LOGR_.tags= ll_;
+console.log('LOGR_.tags', LOGR_.tags);
 
-// LOGR.log(l_.DEL, 'YES');
-// console.log()
+LOGR_.toggled= options.logr;
+console.log('LOGR_.toggled', LOGR_.toggled.toString(2) );
+console.log()
 
-// LOGR.log(l_.MSGE, 'NO');
-// console.log()
+// ----
+
+LOGR_.log(ll_.DEL, 'NO');
+console.log()
+
+LOGR_.log(ll_.CXNS, 'YES');
+console.log()
+
+//-------------------------------------------------------------------------------------------------
+
+// let ignore;
+// let v;
+
+// ignore= 1;
+// v= 0;
+// console.log ( ignore || v );
+
+// ignore= 1;
+// v= 1;
+// console.log ( ignore || v );
+
+// ignore= 0;
+// v= 0;
+// console.log ( ignore || v );
+
+// ignore= 0;
+// v= 1;
+// console.log ( ignore || v );
+
+//-------------------------------------------------------------------------------------------------
