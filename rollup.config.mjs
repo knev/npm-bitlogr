66bf1b84734a2278efe2d88a3356317435f353c3
knev <knev@users.noreply.github.com>
2022-12-14 16:46:27 +0100

    fix: again: let's try just index.d.ts.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,4 @@
 
-import dts from "rollup-plugin-dts";
-
 export default [
 	{
 		input: 'src/logr.mjs',
@@ -17,15 +15,5 @@ export default [
 			file: 'dist/logr.es.mjs',
 			format: 'es'
 		}
-	},
-	{
-		input: "src/logr.mjs.d.ts",
-		output: [{ file: "dist/logr.es.d.ts", format: "es" }],
-		plugins: [dts()],
-	},
-	{
-		input: "src/logr.mjs.d.ts",
-		output: [{ file: "dist/logr.cjs.d.ts", format: "cjs" }],
-		plugins: [dts()],
 	}
 ];
